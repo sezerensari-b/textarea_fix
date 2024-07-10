ff920bf0aa7bd57bb405947247ed608c68533601
sezerensari-b <sezerensari.b@gmail.com>
2024-07-10 08:43:14 +0300

    trimmed_content property added, view changed

--- textareafixer/book/models.py
+++ textareafixer/book/models.py
@@ -4,4 +4,11 @@ class Book(models.Model):
     content = models.TextField()
 
     def __str__(self):
-        return f"Book {self.id}"    
+        return f"Book {self.id}" 
+
+    @property
+    def trimmed_content(self):
+        if len(self.content) >= 4:
+            return self.content[2:-2]
+        else:
+            return self.content
